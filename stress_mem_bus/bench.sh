1203d53a81277fd060a6aec56d207c87937f1b4f
Elazar Leibovich <elazarl@gmail.com>
2016-12-06 20:56:32 +0200

    text in boldface

--- stress_mem_bus/bench.sh
+++ stress_mem_bus/bench.sh
@@ -7,23 +7,28 @@ runecho() {
 	echo $@
 	"$@"
 }
+bold=$(tput rev;tput bold)
+normal=$(tput sgr0)
+boldecho() {
+	echo ${bold}$@${normal}
+}
 
-echo "Let's run a simple benchmark that touches the same memory spot"
+boldecho "Let's run a simple benchmark that touches the same memory spot"
 runecho perf stat ./cachemiss -c 1 -t 10,000,000 | highlight
 read
-echo "See? 4 instructions per cycle! Now with 1,000 cache lines..."
+boldecho "See? 4 instructions per cycle! Now with 1,000 cache lines..."
 runecho perf stat ./cachemiss -c 1,000 | highlight
 read
-echo "Half... L3 is not used so much, we're waiting for memory bus"
+boldecho "Half... L3 is not used so much, we're waiting for memory bus"
 read
-echo "But wait! We might be innocent! let's run 1,000 threads that thrash L3"
+boldecho "But wait! We might be innocent! let's run 1,000 threads that thrash L3"
 runecho ./cachemiss -c 1,000 -t -1
 read
-echo "Now, let's run the same benchmark again, while L3 thrashes"
+boldecho "Now, let's run the same benchmark again, while L3 thrashes"
 read
 runecho perf stat ./cachemiss -c 1 | highlight
-echo "bad!, don't believe me? Let's kill 'em"
+boldecho "bad!, don't believe me? Let's kill 'em"
 killall cachemiss
 read
 runecho perf stat ./cachemiss -c 1 | highlight
-echo "after they die - we're back in business!"
+boldecho "after they die - we're back in business!"
