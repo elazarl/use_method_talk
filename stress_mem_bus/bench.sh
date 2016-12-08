8cd3bef2ed80b7ef3c10107951e8f5b2f5f3ca7b
Elazar Leibovich <elazarl@gmail.com>
2016-12-08 13:20:38 +0200

    more relevant counters

--- stress_mem_bus/bench.sh
+++ stress_mem_bus/bench.sh
@@ -4,14 +4,17 @@ TIMES=100,000,000
 source ../demo.sh
 
 highlight() {
-	grep --color '[^ ]*\s*insns per cycle\|$'
+	grep --color '^.*LLC.*$\|[^ ]*\s*insns per cycle\|$'
 }
+PERF="perf stat -e cycles,instructions,LLC-load-misses,LLC-store-misses"
 
 boldecho "Let's run a simple benchmark that touches the same memory spot"
-runecho perf stat ./cachemiss -c 1 -t $TIMES |& highlight
+read
+git grep -W 'cacheline\[IX.*\]=0'
+runecho $PERF ./cachemiss -c 1 -t $TIMES |& highlight
 read
 boldecho "See? 4 instructions per cycle! Now with 1,000 cache lines..."
-runecho perf stat ./cachemiss -c 1,000 -t $TIMES |& highlight
+runecho $PERF ./cachemiss -c 1,000 -t $TIMES |& highlight
 read
 boldecho "Half... L3 is not used so much, we're waiting for memory bus"
 read
@@ -20,9 +23,9 @@ boldecho "But wait! We might be innocent! let's run 1,000 threads that thrash L3
 read
 boldecho "Now, let's run the same benchmark again, while L3 thrashes"
 read
-runecho perf stat ./cachemiss -c 1 -t $TIMES |& highlight
+runecho $PERF ./cachemiss -c 1 -t $TIMES |& highlight
 boldecho "bad!, don't believe me? Let's kill 'em"
 killall cachemiss
 read
-runecho perf stat ./cachemiss -c 1 -t $TIMES |& highlight
+runecho $PERF ./cachemiss -c 1 -t $TIMES |& highlight
 boldecho "after they die - we're back in business!"
