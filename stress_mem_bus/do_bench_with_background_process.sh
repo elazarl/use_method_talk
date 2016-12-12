3a7ee67efe34e01fc1dbde3e51c4de46479e2241
Elazar Leibovich <elazarl@gmail.com>
2016-12-12 16:58:11 +0200

    correct code for running THREADS threads, PERFCMD to record things with PERF

--- stress_mem_bus/do_bench_with_background_process.sh
+++ stress_mem_bus/do_bench_with_background_process.sh
@@ -6,29 +6,28 @@ highlight() {
 	grep --color '^.*LLC.*miss.*$\|[^ ]*\s*insns per cycle\|$'
 }
 runbackground() {
-	boldecho taskset 0xfffe ./cachemiss -t -1 -T 1 "$@"
-	taskset 0xfffe ./cachemiss -t -1 -T 1 "$@"
+	boldecho taskset 0xfffe ./cachemiss -t -1 -T $THREADS "$@"
+	taskset 0xfffe ./cachemiss -t -1 -T $TIMES "$@"
 }
 TIMES=100,000,000
 THREADS=16
-PERF="perf stat -e context-switches,cycles:k,instructions:k,cycles:u,instructions:u,cache-misses:u,LLC-loads:u,LLC-load-misses:u,LLC-store-misses:u,LLC-stores:u,LLC-loads:k,LLC-load-misses:k,LLC-store-misses:k,LLC-stores:k,L1-dcache-load-misses:u,L1-dcache-loads:u"
-if [ "$1" = "record" ]; then
-	RECORD=1
-	PERF=sudo\ perf\ record\ -F9999
+PERF="perf stat -e context-switches,cycles:k,instructions:k,cycles:u,instructions:u,cache-misses:u,LLC-loads:u,LLC-load-misses:u,LLC-store-misses:u,LLC-stores:u,LLC-loads:k,LLC-load-misses:k,LLC-store-misses:k,LLC-stores:k,L1-dcache-load-misses:u,L1-dcache-loads:u,branch-misses,branch-load-misses,branches,branch-loads"
+if [ -n "$PERFCMD" ]; then
+	PERF="$PERFCMD"
 fi
 trap 'killall ./cachemiss' EXIT
 
 boldecho "But wait! We might be innocent! Let's run again the base benchmark with -c 1"
-runbackground -c 1 -T $THREADS &
+runbackground -c 1 &
 sleep 1
 runecho $PERF ./cachemiss -c 10 -t $TIMES |& highlight
-[ -n "$RECORD" ] && sudo perf report
+[ -n "$PERFCMD" ] && sudo perf report
 boldecho NOTE: last LLC stats are for kernel
 killall ./cachemiss
 read
 boldecho "Now, let's run in a different CPU a process that thrashes the cache"
-runbackground -c 1,000 -A -T $THREADS &
+runbackground -c 1,000 -A &
 sleep 1
 boldecho "and run the exact same benchmark "
 runecho taskset 0x1 $PERF ./cachemiss -c 10 -t $TIMES |& highlight
-[ -n "$RECORD" ] && sudo perf report
+[ -n "$PERFCMD" ] && sudo perf report
