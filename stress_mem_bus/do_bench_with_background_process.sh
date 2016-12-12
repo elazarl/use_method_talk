9f5c82b12176058e491c2e06de7a924abc6a3be1
Elazar Leibovich <elazarl@gmail.com>
2016-12-12 15:29:49 +0200

    separate LLC counters for user and kernel

--- stress_mem_bus/do_bench_with_background_process.sh
+++ stress_mem_bus/do_bench_with_background_process.sh
@@ -10,8 +10,8 @@ runbackground() {
 	taskset 0xfffe ./cachemiss -t -1 -T 1 "$@"
 }
 TIMES=100,000,000
-PERF="perf stat -e context-switches,cycles:k,instructions:k,cycles:u,instructions:u,cache-misses:u,LLC-loads:u,LLC-load-misses:u,LLC-store-misses:u,LLC-stores:u,L1-dcache-load-misses:u,L1-dcache-loads:u"
-PERF="perf stat -e context-switches,cycles:k,instructions:k,cycles:u,instructions:u,cache-misses:u,LLC-loads,LLC-load-misses,LLC-store-misses,LLC-stores,L1-dcache-load-misses,L1-dcache-loads"
+THREADS=16
+PERF="perf stat -e context-switches,cycles:k,instructions:k,cycles:u,instructions:u,cache-misses:u,LLC-loads:u,LLC-load-misses:u,LLC-store-misses:u,LLC-stores:u,LLC-loads:k,LLC-load-misses:k,LLC-store-misses:k,LLC-stores:k,L1-dcache-load-misses:u,L1-dcache-loads:u"
 if [ "$1" = "record" ]; then
 	RECORD=1
 	PERF=sudo\ perf\ record\ -F9999
@@ -19,14 +19,15 @@ fi
 trap 'killall ./cachemiss' EXIT
 
 boldecho "But wait! We might be innocent! Let's run again the base benchmark with -c 1"
-runbackground -c 1 -T 8 &
+runbackground -c 1 -T $THREADS &
 sleep 1
 runecho $PERF ./cachemiss -c 10 -t $TIMES |& highlight
 [ -n "$RECORD" ] && sudo perf report
+boldecho NOTE: last LLC stats are for kernel
 killall ./cachemiss
 read
 boldecho "Now, let's run in a different CPU a process that thrashes the cache"
-runbackground -c 1,000 -A -T 8 &
+runbackground -c 1,000 -A -T $THREADS &
 sleep 1
 boldecho "and run the exact same benchmark "
 runecho taskset 0x1 $PERF ./cachemiss -c 10 -t $TIMES |& highlight
